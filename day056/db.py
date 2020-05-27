3a8ac25929291723524aa3d225783eebd47829be
pywizard@163.com <you@example.com>
2020-05-27 10:44:19 +0800

    clear

--- day056/db.py
+++ day056/db.py
@@ -15,7 +15,7 @@ def sqlite3_demo():
 def SQLAlchemy_demo():
 	from sqlalchemy import create_engine
 	# 创建引擎
-	engine = create_engine("mysql+pymysql://lsy:006261@web4web.top:3306/lsy", max_overflow=5)
+	engine = create_engine("mysql+pymysql://admin:admin@127.0.0.1:3306/mysql", max_overflow=5)
 	# 执行sql语句
 	result = engine.execute("select * from searcher_websites")
 	res = result.fetchall()
@@ -29,7 +29,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String
 # ORM解决中文编码问题 sqlalchemy 默认使用latin-1进行编码。
 # "mysql+pymysql://fuzj:123.com@127.0.0.1:3306/fuzj?charset=utf8"  encoding='utf-8'
-engine = create_engine("mysql+pymysql://lsy:006261@web4web.top:3306/lsy", max_overflow=5,encoding='utf-8')
+engine = create_engine("mysql+pymysql://admin:admin@127.0.0.1:3306/mysql", max_overflow=5,encoding='utf-8')
 
 Base = declarative_base()
 # 绑定数据库引擎
@@ -51,7 +51,7 @@ def SQLAlchemy_ORM_dem(session):
 	# 创建表
 	# Base.metadata.create_all(engine)  # 创建表
 	# Base.metadata.drop_all(engine)   #删除表
-	obj = Webs(name="baidu5", url='http://www.baidu.com5')
+	obj = Webs(name="baidu", url='http://www.baidu.com')
 
 	# 添加数据
 	session.add(obj)
